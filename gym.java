833523ae31cc161230fb019a1c53ee9f5372c7f3
dhanushkavin4765-lang <dhanushkavin4765@gmail.com>
2026-06-25 15:02:04 +0530

    Add files via upload

---
+++ gym.java
@@ -0,0 +1,43 @@
+// Gym Management System
+
+//  Project Description
+
+// This is a simple Java console application for a gym.
+// It checks whether a member is allowed to enter the gym based on **Member ID** and **Fees Expiry Date**.
+
+
+
+// Tools Used
+
+// * Java
+// * Scanner
+// * LocalDate
+
+
+
+// How It Works
+
+// 1. Enter Member ID
+// 2. Enter fees expiry date (YYYY-MM-DD)
+// 3. Scan the Member ID
+// 4. System checks fees status
+
+
+//  Output Rules
+
+// * Correct ID + Fees active → Access Allowed
+// * Correct ID + Fees expired → Renewal needed
+// * Wrong ID → Access Denied
+
+
+
+// Sample Input
+
+// Member ID: 101
+// Expiry Date: 2026-02-15
+// Scan ID: 101
+
+// ## Sample Output
+
+// Fees Active – Access Allowed
+
